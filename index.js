83a77d3bd3015e808f5d8d178e20d20f75cbe37c
Vaibhav Mishra <vmishra175@gmail.com>
2022-06-03 15:40:56 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true })); // limit size of body to 30mb and parse json data from body of request to json object in req.body
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); // its a middleware and limit size of body to 30mb and parse urlencoded data from body of request to json object in req.body
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello to memories api");
+});
+
+// const CONNECTION_URL =
+//   "mongodb+srv://memories:memories@cluster0.mrtz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);   // liking something is like updating the likes each time , so we need to use patch
+
+export default router;
